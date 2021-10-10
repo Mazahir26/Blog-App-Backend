dbad5e35bc80bd385e0d505339b1e7127b1ea287
Mazahir26 <supermazahir@gmail.com>
2021-10-10 14:57:59 +0530

    Added Token Auth

--- Notifi/settings.py
+++ Notifi/settings.py
@@ -40,7 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'API',
+    'rest_framework.authtoken',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
